9f1a0e00eb468386601ae76c984f65cb3668a4cf
Ádám Szilágyi <szilagyi.adam4@gmail.com>
2023-08-27 18:36:18 +0200

    feat: Initial data, db setup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FlightData.DAL;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddTransient<FlightDataContext>();
+
+using IHost host = builder.Build();
+host.RunAsync();
+
+var dbContext = host.Services.GetRequiredService<FlightDataContext>();
+
+var seeder = new Seed(dbContext);
+seeder.SeedDatabase();
+
+Console.WriteLine("Hello FlightRadar!");

644754639ef6544943afd19c8d4849e633f0cb14
Ádám Szilágyi <szilagyi.adam4@gmail.com>
2023-08-29 18:59:03 +0200

    feature: services, initial program out lines

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using FlightData.DAL;
+﻿using FlightData.BLL.Interfaces;
+using FlightData.BLL.MapperProfiles;
+using FlightData.BLL.Services;
+using FlightData.DAL;
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -6,13 +9,26 @@ using Microsoft.Extensions.Hosting;
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddTransient<FlightDataContext>();
+builder.Services.AddAutoMapper(typeof(FlightDataProfile));
+builder.Services.AddTransient<IAirlineServices, AirlineServices>();
+builder.Services.AddTransient<ICityServices, CityServices>();
+builder.Services.AddTransient<IFlightServices, FlightService>();
+builder.Services.AddTransient<IFlightPlannerService, FlightPlannerService>();
 
 using IHost host = builder.Build();
 host.RunAsync();
 
 var dbContext = host.Services.GetRequiredService<FlightDataContext>();
+var cityServices = host.Services.GetRequiredService<ICityServices>();
+var airlineServices = host.Services.GetRequiredService<IAirlineServices>();
+var flightServices = host.Services.GetRequiredService<IFlightServices>();
+var flightPlannerService = host.Services.GetRequiredService<IFlightServices>();
 
 var seeder = new Seed(dbContext);
 seeder.SeedDatabase();
 
-Console.WriteLine("Hello FlightRadar!");
+var smallestCity = await cityServices.GetSmallestCityAsync();
+var biggestCity = await cityServices.GetBiggestCityAsync();
+
+Console.WriteLine($"Legkisebb város: {smallestCity.Name}, {smallestCity.Population} lakos");
+Console.WriteLine($"Legnagyobb város: {biggestCity.Name}, {biggestCity.Population} lakos");
